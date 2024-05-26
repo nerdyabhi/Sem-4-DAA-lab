09608c824cbc6048bb92f17ef79b40c8a7164c40
Abhi Sharma <116084412+nerdyabhi@users.noreply.github.com>
2024-05-26 18:27:36 +0530

    Update Quick-sort-DNC.c
    Changed it from C++ to C and added a testcase

--- Divide-and-Conquer/Quick-sort-DNC.c
+++ Divide-and-Conquer/Quick-sort-DNC.c
@@ -1,33 +1,49 @@
-void quickSort(int arr[], int s, int e)
-    {
-        // code here
-        
-        if(s>=e) return;
-        
-        int p = partition(arr , s , e );
-        
-        quickSort(arr , s , p-1); // left
-        
-        quickSort(arr , p+1 , e); //right
-    }
-    
-    public:
-    int partition (int arr[], int s, int e)
-    {
-       int pivot = arr[e];
+// Online C compiler to run C program online
+#include <stdio.h>
+void swap(int*a , int*b)
+{
+    int temp = *a ;
+    *a = *b;
+    *b = temp;
+}
 
-    int pivot_index = s;
-    for (int j = s; j < e; j++)
+int partition(int arr[] , int s , int e)
+{
+    int pivot = arr[e];
+    int index = s;
+    
+    for(int i=s; i<e; i++)
     {
-        if (arr[j] < pivot)
+        if(arr[i] < pivot)
         {
-            swap(arr[pivot_index], arr[j]);
-            pivot_index++;
-        } 
+            swap(&arr[i], &arr[index]);
+            index++;
+        }
     }
-    swap(arr[pivot_index], arr[e]);
-
-    return pivot_index;
     
+    swap(&arr[e] , &arr[index]);
+   
+    return index;
+}
+void quickSort(int arr[] , int s , int e)
+{
+    if(s<e)
+    {
+        int p = partition(arr , s , e);
+        quickSort(arr , s , p-1);
+        quickSort(arr , p+1 , e);
     }
-};
+}
+
+int main() {
+    // Write C code here
+    int arr[] = {10 , 9 , 8 , 6, 5, 3, 2, 1, 0 , -2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    quickSort(arr , 0 ,n-1 );
+    
+    for(int i =0; i<n; i++) printf("%d\t" , arr[i]);
+    printf("\n");
+
+    return 0;
+}
